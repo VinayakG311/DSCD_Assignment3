16e03e278d81dfc3fbcb5c5dc488a6fdfa9b7f8b
Vinayak Goel <vinayak21113@iiitd.ac.in>
2024-04-12 19:14:09 +0530

    try

--- Kmeans_pb2.pyi
+++ Kmeans_pb2.pyi
@@ -1,4 +1,64 @@
+from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
-from typing import ClassVar as _ClassVar
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
+
+class Centroids(_message.Message):
+    __slots__ = ("data",)
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    data: float
+    def __init__(self, data: _Optional[float] = ...) -> None: ...
+
+class Data(_message.Message):
+    __slots__ = ("key", "value")
+    KEY_FIELD_NUMBER: _ClassVar[int]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    key: float
+    value: float
+    def __init__(self, key: _Optional[float] = ..., value: _Optional[float] = ...) -> None: ...
+
+class MasterToMapperReq(_message.Message):
+    __slots__ = ("path", "start_index", "end_index", "prev_Centroids")
+    PATH_FIELD_NUMBER: _ClassVar[int]
+    START_INDEX_FIELD_NUMBER: _ClassVar[int]
+    END_INDEX_FIELD_NUMBER: _ClassVar[int]
+    PREV_CENTROIDS_FIELD_NUMBER: _ClassVar[int]
+    path: str
+    start_index: int
+    end_index: int
+    prev_Centroids: _containers.RepeatedCompositeFieldContainer[Centroids]
+    def __init__(self, path: _Optional[str] = ..., start_index: _Optional[int] = ..., end_index: _Optional[int] = ..., prev_Centroids: _Optional[_Iterable[_Union[Centroids, _Mapping]]] = ...) -> None: ...
+
+class MasterToMapperRes(_message.Message):
+    __slots__ = ("success",)
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    success: int
+    def __init__(self, success: _Optional[int] = ...) -> None: ...
+
+class MasterToReducerReq(_message.Message):
+    __slots__ = ("start_process",)
+    START_PROCESS_FIELD_NUMBER: _ClassVar[int]
+    start_process: int
+    def __init__(self, start_process: _Optional[int] = ...) -> None: ...
+
+class MasterToReducerRes(_message.Message):
+    __slots__ = ("success",)
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    success: int
+    def __init__(self, success: _Optional[int] = ...) -> None: ...
+
+class ReducerToMapperReq(_message.Message):
+    __slots__ = ("id",)
+    ID_FIELD_NUMBER: _ClassVar[int]
+    id: int
+    def __init__(self, id: _Optional[int] = ...) -> None: ...
+
+class ReducerToMapperRes(_message.Message):
+    __slots__ = ("data", "success")
+    DATA_FIELD_NUMBER: _ClassVar[int]
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    data: _containers.RepeatedCompositeFieldContainer[Data]
+    success: int
+    def __init__(self, data: _Optional[_Iterable[_Union[Data, _Mapping]]] = ..., success: _Optional[int] = ...) -> None: ...
--- Kmeans_pb2_grpc.py
+++ Kmeans_pb2_grpc.py
@@ -2,6 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
+import kmeans_pb2 as kmeans__pb2
 
 
 class KmeansStub(object):
@@ -13,14 +14,62 @@ class KmeansStub(object):
         Args:
             channel: A grpc.Channel.
         """
+        self.MasterToMapper = channel.unary_unary(
+                '/Kmeans/MasterToMapper',
+                request_serializer=kmeans__pb2.MasterToMapperReq.SerializeToString,
+                response_deserializer=kmeans__pb2.MasterToMapperRes.FromString,
+                )
+        self.MasterToReducer = channel.unary_unary(
+                '/Kmeans/MasterToReducer',
+                request_serializer=kmeans__pb2.MasterToReducerReq.SerializeToString,
+                response_deserializer=kmeans__pb2.MasterToReducerRes.FromString,
+                )
+        self.ReducerToMapper = channel.unary_unary(
+                '/Kmeans/ReducerToMapper',
+                request_serializer=kmeans__pb2.ReducerToMapperReq.SerializeToString,
+                response_deserializer=kmeans__pb2.ReducerToMapperRes.FromString,
+                )
 
 
 class KmeansServicer(object):
     """Missing associated documentation comment in .proto file."""
 
+    def MasterToMapper(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def MasterToReducer(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def ReducerToMapper(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_KmeansServicer_to_server(servicer, server):
     rpc_method_handlers = {
+            'MasterToMapper': grpc.unary_unary_rpc_method_handler(
+                    servicer.MasterToMapper,
+                    request_deserializer=kmeans__pb2.MasterToMapperReq.FromString,
+                    response_serializer=kmeans__pb2.MasterToMapperRes.SerializeToString,
+            ),
+            'MasterToReducer': grpc.unary_unary_rpc_method_handler(
+                    servicer.MasterToReducer,
+                    request_deserializer=kmeans__pb2.MasterToReducerReq.FromString,
+                    response_serializer=kmeans__pb2.MasterToReducerRes.SerializeToString,
+            ),
+            'ReducerToMapper': grpc.unary_unary_rpc_method_handler(
+                    servicer.ReducerToMapper,
+                    request_deserializer=kmeans__pb2.ReducerToMapperReq.FromString,
+                    response_serializer=kmeans__pb2.ReducerToMapperRes.SerializeToString,
+            ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
             'Kmeans', rpc_method_handlers)
@@ -30,3 +79,54 @@ def add_KmeansServicer_to_server(servicer, server):
  # This class is part of an EXPERIMENTAL API.
 class Kmeans(object):
     """Missing associated documentation comment in .proto file."""
+
+    @staticmethod
+    def MasterToMapper(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Kmeans/MasterToMapper',
+            kmeans__pb2.MasterToMapperReq.SerializeToString,
+            kmeans__pb2.MasterToMapperRes.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def MasterToReducer(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Kmeans/MasterToReducer',
+            kmeans__pb2.MasterToReducerReq.SerializeToString,
+            kmeans__pb2.MasterToReducerRes.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+    @staticmethod
+    def ReducerToMapper(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Kmeans/ReducerToMapper',
+            kmeans__pb2.ReducerToMapperReq.SerializeToString,
+            kmeans__pb2.ReducerToMapperRes.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
