c3c2a78d85823d158bdac32d0e52423e9baf13a5
vinayak-77 <vinayak270603@gmail.com>
2024-04-20 03:38:43 +0530

    Reducer resolve

--- ReducerInfo.py
+++ ReducerInfo.py
@@ -1,10 +1,14 @@
-import sys
-
 import grpc
+import sys
+import threading
+import time
+from concurrent import futures
+from Servicer import KmeansServicer
 
+import Kmeans_pb2 as Kmeans_pb2
 import Kmeans_pb2_grpc as Kmeans_pb2_grpc
-
-ip = sys.argv[1]
+masterIP = "127.0.0.1:50051"
+port = sys.argv[1]
 class Reducer():
     name=""
     ip=""
@@ -17,7 +21,40 @@ class Reducer():
         pass
 
 
+t = []
 def run():
-    reducer = Reducer(name="",ip=ip)
-    with grpc.insecure_channel(ip) as channel:
+    # mapper = Mapper(name="mapper",ip=f"[::]:{port}")
+    with grpc.insecure_channel(masterIP) as channel:
         stub = Kmeans_pb2_grpc.KmeansStub(channel)
+        # print("STUB CREATED")
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+    Kmeans_pb2_grpc.add_KmeansServicer_to_server(KmeansServicer(), server)
+    server.add_insecure_port(f"127.0.0.1:{port}")
+    server.start()
+    
+    try:
+        while True:
+                time.sleep(3600)  # One hour
+    except KeyboardInterrupt:
+        
+        server.stop(0)
+        sys.exit(0)
+    
+    
+if __name__ == '__main__':
+    t1 = threading.Thread(target=run)
+    t2 = threading.Thread(target=serve)
+    
+    t.append(t2)
+    t.append(t1)
+    
+    try:
+        for i in t:
+            i.start()
+        for i in t:
+            i.join()
+
+    except KeyboardInterrupt:
+        sys.exit(0)
--- Servicer.py
+++ Servicer.py
@@ -18,6 +18,8 @@ reducer_port = ["127.0.0.1:50059", "127.0.0.1:50060", "127.0.0.1:50061", "127.0.
 data_mapper = {}
 centroids_mapper=[]
 
+M = 2
+
 def MapperPartition(ind, data, centroids, reducer_count):
     # If has to be read from that
     # reader = open("Data/Mappers/M"+str(ind)+"/data.txt","r")
@@ -39,14 +41,25 @@ def MapperPartition(ind, data, centroids, reducer_count):
         f.close()
 
 
-def shuffle_and_sort(data):
-    print("shuffle and sort")
+def shuffle_and_sort(finalData,currData):
+    for data in currData:
+        finalData[data[0]].append([data[1],data[2]])
+    
+    finalData = dict(sorted(finalData.items()))
+    return finalData
 
 
-def Reduce():
-    print("Reduce")
+def Reduce(key,points):
+    count = 0
+    xSum = 0
+    ySum = 0
+    
+    for point in points:
+        xSum+=point[0]
+        ySum+=point[1]
+        count+=1
 
-
+    return [key,[xSum/count,ySum/count]]
 class KmeansServicer(Kmeans_pb2_grpc.KmeansServicer):
     def MasterToMapper(self, request, context):
         print(f"Request received{request}")
@@ -76,47 +89,88 @@ class KmeansServicer(Kmeans_pb2_grpc.KmeansServicer):
             else:
                 dict[min_ind] = [point]
         path = "Data/Mappers/M" + str(request.mapper_index)
-        if not os.path.isdir(path):
-            os.mkdir(path)
+        if not os.path.exists(path):
+            os.makedirs(path)
 
         writer = open(path + "/Data.txt", "w")
         datas = []
         for k, v in dict.items():
             for j in v:
-                writer.write(str(k) + " " + str(j[0]) + " " + str(j[1]) + "\n")
-                datas.append([k, j[0], j[1]])
+                writer.write(str(k+1) + " " + str(j[0]) + " " + str(j[1]) + "\n")
+                datas.append([k+1, j[0], j[1]])
         writer.close()
         MapperPartition(request.mapper_index, datas, centroids, request.reducer_count)
         return Kmeans_pb2.MasterToMapperRes(success=1)
         # return super().MasterToMapper(request, context)
 
-    def ReducerToMapper(self, request, context):
+    def ReducerToMapper(self, request,context):
         id=request.id
-        data=data_mapper[id]
+        data=[]
         reducer_Data=[]
-        centroid_id=data[0][0]
+        numMappers = M
+        for i in range(numMappers):
+            with open(f"Data/Mappers/M{i}/Data.txt","r") as f:
+                datapoints = f.readlines()
+                
+                for i in range(len(datapoints)):
+                    # print(point)
+                    # point = point.split(" ")
+                    point = datapoints[i].split(" ")
+                    data.append([int(point[0]),float(point[1]),float(point[2])])
+        # centroid_id=data[0][0]
         for i in data:
-            d=Kmeans_pb2.Data(key=i[1],value=i[2])
+            point = Kmeans_pb2.Point(x = i[1],y = i[2])
+            d=Kmeans_pb2.Data(key=i[0],point=point)
             reducer_Data.append(d)
 
-        return Kmeans_pb2.ReducerToMapperRes(success=1,data=reducer_Data,centroid=centroids_mapper[centroid_id],centroid_id=centroid_id)
+        return Kmeans_pb2.ReducerToMapperRes(success=1,data=reducer_Data)
 
     def MasterToReducer(self, request, context):
+        M = request.M
         if request.start_process != 1:
             print("Error")
             return Kmeans_pb2.MasterToReducerRes(success=0)
         # Getting data from mapper
-        data = []
+        finalData = {}
         for i in mapper_port:
-            with grpc.insecure_channel(i) as channel:
-                stub = Kmeans_pb2_grpc.KmeansStub(channel)
-                req = Kmeans_pb2.ReducerToMapperReq(id=request.id)
-                res = stub.ReducerToMapper(req)
-                d = res.data
-                keyval = []
-                for i in d:
-                    keyval.append([i.key, i.value])
-                data.append(keyval)
-        shuffle_and_sort(data)
+            try:
+                with grpc.insecure_channel(i) as channel:
+                    stub = Kmeans_pb2_grpc.KmeansStub(channel)
+                    req = Kmeans_pb2.ReducerToMapperReq(id=request.id)
+                    res = stub.ReducerToMapper(req)
+                    d = res.data
+                    # print(d)
+                    currData = []
+                    for j in d:
+                        # print(j)
+                        currData.append([j.key,j.point.x,j.point.y])
+                        # print(currData)
+                    # print(currData)
+                    # print(len(currData))
+                    for j in currData:
+                        # print(j)
+                        if(j[0] not in finalData):
+                            finalData[j[0]] = []
+                        finalData[int(j[0])].append([j[1],j[2]])
+                        # print(finalData)
+                    # print("THIS - > ",finalData)
+                    finalData = dict(sorted(finalData.items()))
+                    # finalData = shuffle_and_sort(finalData,currData)
+            except:
+                
+                break
+                # data.append(keyval)
+            
+        print(finalData)
+        for data in finalData:
+            key = data
+            points = finalData[key]
+            reducerNum,centroid = Reduce(key,points)
 
-        return Kmeans_pb2.MasterToReducerRes(success=0)
+            path = "Data/Reducers/"
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            writer = open(f"{path}R{reducerNum}.txt", "w")
+            writer.write(f"{key} {centroid[0]} {centroid[1]}")
+        return Kmeans_pb2.MasterToReducerRes(success=1)
